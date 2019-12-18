99232f27359f3bc0e8b89b61c6ca793b9cc62f5c
jackli-boop <58491392+jackli-boop@users.noreply.github.com>
2019-12-17 23:38:17 -0800

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -10,4 +10,5 @@ if(isset($_POST['submit'])) {
 	$txt = "You have revieced an email from".$name.".\n\n".$message;
 	
 	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.html?mailsend");
 }
